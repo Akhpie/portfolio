33b652ecb2ab8fd99b9d5fc481f86fa8ce868c46
Akhpie <134225827+Akhpie@users.noreply.github.com>
2024-05-17 23:58:19 +0530

    new ui

--- prof.js
+++ prof.js
@@ -1,31 +1,38 @@
-$('.resum').hover(
-    function(){
-        $('.imageee').fadeIn('slow')
-    },function(){
-        $('.imageee').fadeOut('slow')
-    }
+$(".resum").hover(
+  function () {
+    $(".imageee").fadeIn("slow");
+  },
+  function () {
+    $(".imageee").fadeOut("slow");
+  }
 );
 
-$(document).ready(function() {
-    $("#carousel").slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-        nextArrow: '<button type="button" class="slick-next">Next</button>',
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                }
-            }
-        ]
-    });
+$(document).ready(function () {
+  $("#carousel").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-next">Next</button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
+
+document.getElementById("nav-toggle").addEventListener("click", function () {
+  this.classList.toggle("active");
+  var navLinks = document.getElementById("nav-links");
+  navLinks.classList.toggle("active");
 });
